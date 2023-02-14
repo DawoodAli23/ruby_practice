8e995996a89719d11f35668f8bbfe82536a05548
DawoodAli23 <tatwatatwa007@gmail.com>
2023-02-14 17:48:35 +0500

    completed https://leetcode.com/problems/first-missing-positive

---
+++ 41-firstMIssingPoitive.rb
@@ -0,0 +1,15 @@
+def first_missing_positive(nums=[])
+  hash = {}
+  nums.map { |i| hash[i] = hash[i] ? ++hash[i]:1 }
+  i=1
+  loop do
+    if !hash.has_key?(i)
+      break
+    end
+    i+=1
+  end
+  i
+end
+
+
+puts first_missing_positive [3,4,-1,1]
